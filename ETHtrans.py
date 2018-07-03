8ad8b2ad2124ccb3b4a9dad14829e1cde0238dfa
Lawrence Li <lawrenceli@Lawrences-MacBook-Pro.local>
2018-07-03 10:53:00 +0200

    ethereum transaction

--- ETHtrans.py
+++ ETHtrans.py
@@ -9,10 +9,10 @@ import random
 app = Flask(__name__)
 
 
-FROM_ADDR = "0xde055eCaB590E0E7f2Cb06445dd6272fb7D65129"
-TO_ADDR #= "0x6F544455D57caA0787A5200DA1FC379fc00B5Da5"
-PRIV_KEY = "8c70afd6be9a772cd1fe852c411cc67b829f402c733a45d27b9b8eb6b9710dc4"
-VAL = random.randint(10000, 10000000)
+# FROM_ADDR = "0xde055eCaB590E0E7f2Cb06445dd6272fb7D65129"
+# TO_ADDR = "0x6F544455D57caA0787A5200DA1FC379fc00B5Da5"
+# PRIV_KEY = "8c70afd6be9a772cd1fe852c411cc67b829f402c733a45d27b9b8eb6b9710dc4"
+# VAL = random.randint(10000, 10000000)
 
 web3 = Web3(HTTPProvider('https://rinkeby.infura.io/UVgPTn3TgFMB0KhHUlif'))
 if web3.isConnected():
@@ -33,16 +33,14 @@ def construct_tx(from_addr, to_address, val):
     }
     return txparams
 
-def send_eth(from_addr, to_address, val):
+def send_eth(from_addr, to_address, val, priv_key):
     tx = construct_tx(from_addr, to_address, val) # this generates the transaction dict
     print(tx)
 
     web3.eth.enable_unaudited_features()
 
-    signed_tx = web3.eth.account.signTransaction(tx, PRIV_KEY)  # this returns a blob of hexadecimal text
-    print(signed_tx.rawTransaction)
+    signed_tx = web3.eth.account.signTransaction(tx, priv_key)  # this returns a blob of hexadecimal text
 
-    web3.eth.sendRawTransaction(signed_tx.rawTransaction)  # this broadcasts the tx and returns a transaction hash
+    #web3.eth.sendRawTransaction(signed_tx.rawTransaction)  # this broadcasts the tx and returns a transaction hash
 
-if __name__ == '__main__':
-    send_eth(FROM_ADDR, TO_ADDR, VAL)
+    return web3.toHex(signed_tx.rawTransaction)
--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from redis import Redis, RedisError
 from ETHgen import sk, pk, ad 
 from BTCtrans import BTC_process
+from ETHtrans import send_eth
 import os
 import socket  
 import random
@@ -20,32 +21,49 @@ def hello():
 
     try: 
         amnt = float(random.randrange(1, 500))/100
-        amount = amnt
+        amount = amnt/1000
         dest = 'mrHXbzTszNWhav7egmfXVktopTBMotS4mp'
         fee = 8000
         tx = BTC_process(destination=dest, priv_wif='93NUtNNeKfpPZTtB6dEBxjPhBBs8ksYZnHh26RuB8Xe9QUychy6', fee=fee)
-        fl = ""
+        btcfl = ""
     except:
         tx = "unable to make transaction"
         amount = "unable to make transaction"
         dest = "unable to make transaction"
-        fl = "(Failed)"
+        btcfl = "(Failed)"
+
+    try:
+        val = random.randint(10000, 10000000)
+        from_address = '0xde055eCaB590E0E7f2Cb06445dd6272fb7D65129'
+        to_address = '0x6F544455D57caA0787A5200DA1FC379fc00B5Da5'
+        priv_key = '8c70afd6be9a772cd1fe852c411cc67b829f402c733a45d27b9b8eb6b9710dc4'
+        ethtx = send_eth(from_address, to_address, val, priv_key)
+        ethfl = ''
+    except:
+        ethtx = "unable to make transaction"
+        val = "unable to make transaction"
+        to_address = "unable to make transaction"
+        ethfl = "(Failed)"
 
     html = "<b>Hostname:</b> {hostname}<br/><br/>" \
            "<h2><u>ETH Wallet Details</u></h2><br/>"\
            "<b>Public Key:</b> <details>{pk}</details><br/>" \
            "<b>Private Key:</b> <details>{sk}</details><br/>" \
            "<b>Address:</b> <details>{ad}</details><br/><br/>" \
-           "<h2><u>BTC Transaction Details</u>{fl}</h2><br/>"\
+           "<h2><u>BTC Transaction Details</u>{btcfl}</h2><br/>"\
            "<b>Destination Address:</b> <details>{dest}</details><br/>" \
-           "<b>Amount:</b> <details>{amount} mbtc</details><br/>" \
+           "<b>Amount:</b> <details>{amount} BTC</details><br/>" \
            "<b>TX:</b> <details>{tx}</details><br/><br/>" \
+           "<h2><u>ETH Transaction Details</u>{ethfl}</h2><br/>"\
+           "<b>Destination Address:</b> <details>{to_address}</details><br/>" \
+           "<b>Amount:</b> <details>{val} wei</details><br/>" \
+           "<b>TX:</b> <details>{ethtx}</details><br/><br/>" \
         #    "<h2><u>BTC Wallet Details</u></h2><br/>"\
         #    "<b>Public Key:</b> {Bpk}<br/>" \
         #    "<b>Private Key:</b> {Bsk}<br/>" \
         #    "<b>Address:</b> {Badd}<br/><br/>" \
         #    "<b>Visits:</b> {visits}"
-    return html.format(pk=pk, ad=ad, sk=sk, hostname=socket.gethostname(), visits=visits, dest=dest, amount=amount, tx=tx, fl=fl)
+    return html.format(pk=pk, ad=ad, sk=sk, hostname=socket.gethostname(), visits=visits, dest=dest, amount=amount, tx=tx, btcfl=btcfl, to_address=to_address, val=val, ethtx=ethtx, ethfl=ethfl)
     #Bpk=Bpk, Bsk=Bsk, Badd=Badd,
 
 if __name__ == "__main__":
