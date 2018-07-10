0fa8095340be69d1860265ced4c47cbeb74f1260
Lawrence Li <lawrenceli@Lawrences-MacBook-Pro.local>
2018-07-10 14:56:34 +0200

    travis ci 10

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+#from flask import Flask, render_template
 from BTCtrans import BTC_process
 from ETHtrans import send_eth
 from GenAddrs import full_wallets, xpub_eth, XPUB_btc
