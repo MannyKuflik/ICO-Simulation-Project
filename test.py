619f5d8405b3d8f092674a5982ca973d1c481f03
Lawrence Li <lawrenceli@Lawrences-MacBook-Pro.local>
2018-07-18 10:54:48 +0200

    refactoring BTC

--- test.py
+++ test.py
@@ -45,7 +45,7 @@ class TestBitcoin(TestCase):
         assert BTCtrans.round_sig(1234560, 4) == 1235000
         assert BTCtrans.round_sig(0.000456, 2) == 0.00046
 
-    def test_create_transaction(self):
+    def test_create_transaction():
         assert mock_create_transaction(destination, destsamnts, key, fee=5) == '010000000126651c0bfea1f7f471f06deac7aeb036c2ee70271e04ff3d91127bd443bfbfdb010000008b48304502210086b9842ccb485378f3ef7a0627e8cbde4c2100258f6d677ef5b96259f467be2c022048c341b2d158ab61ef08ed62ca9a04f204b76cd4c07dbff898e1ae2792cf804e0141049b3249d74128dd68277a63c11cb09956dcc167904e8a6993779abaa416fbff22f5558d59c17aa836013b8a43c425aacfc19b96011cebc85fd1e7a7a390a23636ffffffff0380f0fa02000000001976a914663bfb4ddb23d714258cf9e7c868da5aade7a31e88ac80f0fa02000000001976a9140b3d83be5c38c473c2aefc14095284d0dc6555f288ac3b1c7215000000001976a9144b820d01817f597d072a333776f4d672499ed57388ac00000000'
 
     # def test_success_broadcast():
@@ -60,11 +60,12 @@ class TestBitcoin(TestCase):
     
     @patch('BTCtrans.broadcast')
     def test_addtoBTCS(self, mock_broadcast):
-        mock_broadcast.return_value = (<Response [200]>, [('mzrhmQRGKMCgx3o1PYY4DcHW5FgZPFZk9N', 2.9e-07), ('mzrhmQRGKMCgx3o1PYY4DcHW5FgZPFZk9N', 4.33e-07), ('mzrhmQRGKMCgx3o1PYY4DcHW5FgZPFZk9N', 3.53e-07), ('mzrhmQRGKMCgx3o1PYY4DcHW5FgZPFZk9N', 2.83e-07), ('mpYCitAEcTYXr6ayqjvioxFngBW8MhM48U', 1.94e-07)], True)
+        mock_broadcast.return_value = 12 #(<Response [200]>, [('mzrhmQRGKMCgx3o1PYY4DcHW5FgZPFZk9N', 2.9e-07), ('mzrhmQRGKMCgx3o1PYY4DcHW5FgZPFZk9N', 4.33e-07), ('mzrhmQRGKMCgx3o1PYY4DcHW5FgZPFZk9N', 3.53e-07), ('mzrhmQRGKMCgx3o1PYY4DcHW5FgZPFZk9N', 2.83e-07), ('mpYCitAEcTYXr6ayqjvioxFngBW8MhM48U', 1.94e-07)], True)
         x = BTCtrans.addtoBTCS
         print(x)
 
 if __name__ == "__main__":
-    TestBitcoin.test_round_sig()
-    TestBitcoin.test_create_transaction(mock_create_transaction)
-    TestBitcoin.test_failed_broadcast()
+    # TestBitcoin.test_round_sig()
+    # TestBitcoin.test_create_transaction()
+    # TestBitcoin.test_failed_broadcast()
+    TestBitcoin.test_addtoBTCS()
